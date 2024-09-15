7b2010eb4309aa69ec23a3f9137c56393edd3c08
Egor Lyakhovets <em@lyakhovets.com>
2024-09-16 01:18:21 +0700

    second dimention runge

--- headers/runge.h
+++ headers/runge.h
@@ -15,7 +15,16 @@ public:
         std::function<double(double, double)> func,
         double y0,
         double x0,
-        double h,
+        double h = 0.1,
+        std::size_t n = 100
+    );
+
+    static std::pair<std::vector<double>, std::vector<double>> solve(
+        std::function<double(double, double, double)> func,
+        double y0,
+        double dy0,
+        double x0,
+        double h = 0.1,
         std::size_t n = 100
     );
 
--- src/runge.cpp
+++ src/runge.cpp
@@ -29,4 +29,46 @@ Runge::solve(
     }
 
     return std::pair{x, y};
+};
+
+std::pair<std::vector<double>, std::vector<double>>
+Runge::solve(
+    std::function<double(double, double, double)> func,
+        double y0,
+        double dy0,
+        double x0,
+        double h,
+        std::size_t n
+) {
+
+    std::vector<double> y(n);
+    std::vector<double> x(n);
+
+    double k1 = 0., k2 = 0., k3 = 0., k4 = 0.;
+    double half_h = h / 2.;
+    double d_y = 0.;
+    y[0] = y0;
+    x[0] = x0;
+    
+    for (std::size_t i = 1; i < n; ++i)
+    {
+        k1 = h * func(x[i-1], y[i-1], dy0);
+        k2 = h * func(
+            x[i-1] + half_h,
+            y[i-1] + half_h * dy0 + (h / 8.) * k1,
+            dy0 + k1 / 2.);
+        k3 = h * func(
+            x[i-1] + half_h,
+            y[i-1] + half_h * dy0 + h / 8. * k1,
+            dy0 * k2 / 2.);
+        k4 = h * func(
+            x[i-1] + h,
+            y[i-1] + h * dy0 + half_h * k3,
+            dy0 * k3);
+        y[i] = y[i-1] + h * (dy0 + (k1 + k2 + k3) / 6.);
+        dy0 += (k1 + 2 * (k2 + k3) + k4) / 6.;
+        x[i] = x[i-1] + h;
+    }
+
+    return std::pair{x, y};
 }
